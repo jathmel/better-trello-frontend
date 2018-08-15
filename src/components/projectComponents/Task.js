cdc04feaa8a1ecb758456bc142d73520851bcfd5
jathmel <jathmel@gmail.com>
2018-08-14 22:49:40 -0400

    display task

--- src/components/projectComponents/Task.js
+++ src/components/projectComponents/Task.js
@@ -1,2 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
+
+const Task = props => {
+
+
+    console.log(props.task);
+    return(
+      <div>
+        <h2>{props.task.title}</h2>
+      </div>
+    )
+
+}
+
+export default (Task)
--- src/components/projectComponents/TaskList.js
+++ src/components/projectComponents/TaskList.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import Task from './Task'
 
+
 class TaskList extends Component {
   componentDidMount() {
     this.props.tasks
   }
    render(){
-     console.log( 'in tasklist', this.props);
+     console.log( 'in tasklist', this.props.selectedProject);
+     const task = this.props.tasks.map(task => {
+       return <Task key={task.id} task={task}/>})
      return(
 
-
        <div>
-        <h1>Hello from task list</h1>
+        <h1>{this.props.selectedProject.name}</h1>
+        <div>{task}</div>
        </div>
      )
    }
 }
 
 const mapStateToProps = (state) =>{
-  return{
-    tasks: state.tasks
+  return {
+    tasks: state.tasks,
+    selectedProject: state.selectedProject
   }
 }
 
-export default (TaskList)
+export default connect(mapStateToProps)(TaskList)
 // const task = this.props.tasks.map(task = {
 //   return <Task key={task.id} task={task}/>
